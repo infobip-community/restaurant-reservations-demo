72cb36e9671aad97109ca7b681b733d29bfede28
Jeaneth Farmer <zfarmer@infobip.com>
2022-11-08 16:55:49 -0600

    oauth

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -29,6 +29,7 @@ import {
 import oauthService from "../services/oauth";
 
 const App = () => {
+  console.log("oauth");
   const oauthEnabled = process.env["REACT_APP_OAUTH_ACTIVE"] === "true";
   const { authService } = useAuth();
   const [alert, setAlert] = useState<AlertI>(defaultAlertContextValue);
