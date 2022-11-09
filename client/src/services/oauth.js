dbd53cab03141a7ea14d7be97f5ab3f064454bb5
Jeaneth Farmer <zfarmer@infobip.com>
2022-11-08 22:47:44 -0600

    try diff urls

--- client/src/services/oauth.js
+++ client/src/services/oauth.js
@@ -4,8 +4,8 @@ import { AuthService } from "react-oauth2-pkce";
 
 const oauthService = new AuthService({
   clientId: "eaf2e781440e0124f0e7c68a121c0582",
-  // provider: "https://portal.infobip.com/api/amg/exchange/1/oauth",
-  provider: "https://oneapi.infobip.com/exchange/1/oauth",
+  authorizeEndpoint: "https://portal.infobip.com/api/amg/exchange/1/oauth",
+  tokenEndpoint: "https://oneapi.infobip.com/exchange/1/oauth",
   redirectUri: "https://restaurant-reservations-demo-oauth.azurewebsites.net/",
   scopes: ["conversations"],
   location: window.location,
