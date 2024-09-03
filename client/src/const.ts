f34dd717b289d77aae97a7ebfbbf98d688fc95b0
Jan Palko <jan.palko@infobip.com>
2024-09-03 08:59:08 +0200

    SI-2114: Added back possibility to run app with disabled OAuth

--- client/src/const.ts
+++ client/src/const.ts
@@ -3,7 +3,7 @@ export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? '';
 export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI ?? '';
 export const INFOBIP_API_BASE_URL = 'https://' + process.env.REACT_APP_ACCOUNT_DOMAIN_API;
 export const INFOBIP_API_KEY = process.env.REACT_APP_ACCOUNT_API_KEY ?? '';
-export const CONVERSATIONS_INTEGRATION_ENABLED = process.env.REACT_APP_CONVERSATIONS_INTEGRATION_ENABLED === 'true' ?? false;
+export const CONVERSATIONS_INTEGRATION_ENABLED = process.env.REACT_APP_CONVERSATIONS_INTEGRATION === 'true' ?? false;
 
 export const API_RESERVATIONS_PATH = 'exchange/restaurant/reservations';
 export const API_CONFIG_PATH = 'exchange/restaurant/config';
