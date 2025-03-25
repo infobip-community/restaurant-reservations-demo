60cbc2cf45a64d23dd1a319154999db9c594ce32
Jan Palko <jan.palko@infobip.com>
2025-03-25 21:33:36 +0100

    SI-2411: Fixed constants

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,9 +1,9 @@
-export const OAUTH_ACTIVE = process.env.REACT_APP_OAUTH_ACTIVE === 'true' ?? false;
+export const OAUTH_ACTIVE = process.env.REACT_APP_OAUTH_ACTIVE === 'true';
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? '';
 export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI ?? '';
 export const INFOBIP_API_BASE_URL = 'https://' + process.env.REACT_APP_ACCOUNT_DOMAIN_API;
 export const INFOBIP_API_KEY = process.env.REACT_APP_ACCOUNT_API_KEY ?? '';
-export const CONVERSATIONS_INTEGRATION_ENABLED = process.env.REACT_APP_CONVERSATIONS_INTEGRATION === 'true' ?? false;
+export const CONVERSATIONS_INTEGRATION_ENABLED = process.env.REACT_APP_CONVERSATIONS_INTEGRATION === 'true';
 
 export const API_RESERVATIONS_PATH = 'exchange/restaurant/reservations';
 export const API_CONFIG_PATH = 'exchange/restaurant/config';
